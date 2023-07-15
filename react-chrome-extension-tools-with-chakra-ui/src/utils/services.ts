d446533c504bd402af4caec629283fcb8828e855
devlopersabbir <devlopersabbir@gmail.com>
2023-07-15 16:37:37 -0700

    finished

---
+++ react-chrome-extension-tools-with-chakra-ui/src/utils/services.ts
@@ -0,0 +1,9 @@
+export const getSelectedText = () => {
+  const selectedText = document
+    .getSelection()
+    ?.toString()
+    ?.trim()
+    ?.replace(/[^0-9]/g, "")
+    ?.trim();
+  return selectedText;
+};
